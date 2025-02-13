0f8efaca07e0e05c9d078344c7ee5adcef33ebf3
FIC <fic@info.local>
2025-02-12 22:00:21 -0300

    js

---
+++ atividade2/sandbox/script.js
@@ -0,0 +1,51 @@
+console.log("Hello World!");
+
+var idade = 22;
+console.log(idade);
+
+let nome = "lulu";
+console.log(nome);
+
+const cargo = "Professor";
+console.log(cargo);
+
+
+//Exercicio 3
+
+const meuNome = "Renata";
+let minhaIdade = 32;
+
+let msg = `Meu nome é ${meuNome}, tenho ${minhaIdade} anos. `;
+console.log(msg);
+
+console.log(typeof meuNome);
+console.log(typeof minhaIdade);
+
+let bonito = true;
+let feio = false;
+
+let tamanho;
+console.log(tamanho);
+
+let vazio = null;
+console.log(vazio);
+
+//Exercicio - Tipos PRIMITIVOS
+
+let a = 10;
+let b = "Renata";
+let c = "10";
+let d = 20;
+
+console.log(" a + b ", a + b );
+console.log(" a + c ", a + c );
+console.log(" a + d ", a + d );
+
+
+let n1 = "42.52";
+n1 = Number(n1);
+console.log(n1);
+
+
+console.log(parseInt(n1));
+console.log(parseFloat(n1));
